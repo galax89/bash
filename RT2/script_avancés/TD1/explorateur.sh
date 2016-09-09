cf4037af098077cfd57660e59009134b4c6331e2
galax89 <azbn46@gmail.com>
2016-09-10 00:28:34 +0200

    ajout d'une variable chmod. création tableau de droits. Ajout d'algorithme traitant les droits. La condition droit = droit_tableur est à debugger. Optimisation medium effectuée. script fini à 90 %

--- RT2/script_avancés/TD1/explorateur.sh
+++ RT2/script_avancés/TD1/explorateur.sh
@@ -1,33 +1,52 @@
 #!/bin/bash
-chmod 755 explorateur.sh
+chmod 755 ./explorateur.sh
 
-ls -l >> resultats.ls
-#stock arborescence
+#stock de la liste détaillée des fichiers
+ls -l >> /tmp/liste.ls
+
+#lecture de la liste
 cat resultats.ls |
 while read line
 do
-	#récup, nom fichier, owner, type
-	#rep=$(echo $line | cut -s -f1)
+	# On récupère les informations à afficher: nom du fichier, owner, user loggé, type
 	nom=$(echo $line | cut -d" " -f9)
-	user=$(echo $line | cut -d" " -f3)
-	com_file=$(file $nom 2>/dev/null)
-	typ=$(echo $com_file | cut -d":" -f2 2>/dev/null)
+	# owner=$(echo $line | cut -d" " -f3)
+	user=$(whoami)
+	commande_file=$(file $nom 2>/dev/null)
+	typ=$(echo $commande_file | cut -d":" -f2 2>/dev/null)
+	# chmod contient l'affichage des droits + type du fichier
+	chmod=$(echo $line | cut -d" " -f1)
+	# On extrapole les droits de l'utilisateur dabs $droits
+	droits=$(echo $chmod | cut -c2-4)
+	dossier=$(echo $chmod | cut -c1)
+	tableau_droits=('---' '--x' '-w-' '-wx' 'r--' 'r-x' 'rw-' 'rwx')
+	tableau_droits_fr=('rien du tout pour vous, dommage ...' 'éxécution' 'écriture' 'écriture et éxécution' 'lecture et écriture' 'lecture, écriture et éxecution')
 
-	droit=$(echo $line | cut -d" " -f1)
-	dossier=$(echo $droit | cut -c1)
-
-
-	echo "---------"
+	echo -e "\n-------------------------------------------------------------------------"
 	if [ "$dossier" == '-' ]; then
-		echo "Le fichier $nom est un fichier $typ"
+		echo "Le fichier \"$nom\" est un fichier $typ"
 	else
-		echo "Le fichier $nom est un repertoire"
+		echo "Le fichier \"$nom\" est un répertoire"
 	fi
-	echo "$nom est accessible par $user avec les droits $droit"
 
+	# A finir affichage droits
+	# Manque comparaison deux chaine char
+	i=0
+	element=('')
+	for i in $(seq 0 7)
+	do
+		echo "$i $droits"
+		for element in ${tableau_droits[@]}
+		do
+			if [$droits = ${tableau_droits[i]}]; then
+				echo "$droits $tableau_droits"
+				# echo \n "\"$nom\" est accessible par \"$user\" avec les droits d'accès en ${tableau_droits_fr[i]}"
+			fi
+		done
+	done
 done
 
 #-------------------------------
 
-rm resultats.ls
-sleep 4
+rm /tmp/liste.ls
+# sleep 4
